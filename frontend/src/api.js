02e5c8f2afbc79de2ade909e2778e787a9eb1d3b
justinaaronbartlett <144182158+justinaaronbartlett@users.noreply.github.com>
2024-05-24 02:08:33 +0000

    pushing

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -8,3 +8,9 @@ export const createUser = userData => api.post('/users', userData)
 export const getUser = email => api.get(`/users/${email}`)
 export const createPost = postData => api.post('/posts', postData)
 export const getUserPosts = userId => api.get(`/posts/${userId}`)
+
+
+
+// localhost:3000/api/users ==> localhost:5000/api/users
+
+// express ==> flask
