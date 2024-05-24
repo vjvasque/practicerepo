3329c5293d722b7819b10c6fc459cb6838a7a620
justinaaronbartlett <144182158+justinaaronbartlett@users.noreply.github.com>
2024-05-24 02:12:12 +0000

    pushing

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # practicerepo
+
+
+from the root directory, npm install
+from /frontend npm install
+cd backend 
+python -m venv venv
+venv\Scripts\activate
+
+pip install flask flask-pymongo
+
+update app.py to use your mongodb 
--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from routes.users import user_routes
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = ""
+app.config["MONGO_URI"] = "mongodb://localhost:27017/"
 mongo = PyMongo(app)
 
 app.register_blueprint(user_routes, url_prefix='/api')
